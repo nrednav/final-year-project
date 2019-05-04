b6e354767e32d4bdc081e4d7daec181ecb8c8248
nrednav <nrednav@gmail.com>
2019-05-04 18:09:45 +0100

    Created blank files for almost all views Configured the routing to each component in routes.js

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,8 +1,7 @@
 function redirect_if_logged_in(to, from, next) {
 	if (localStorage.token) {
 		next('/select-profile')
-	}
-	else {
+	} else {
 		next()
 	}
 }
@@ -10,8 +9,7 @@ function redirect_if_logged_in(to, from, next) {
 function logged_in(to, from, next) {
 	if (localStorage.token) {
 		next()
-	}
-	else {
+	} else {
 		next('/login')
 	}
 }
@@ -41,6 +39,48 @@ export const routes = [
 		component: () => import('@/views/both/Dashboard.vue'),
 		beforeEnter: logged_in
 	},
+	{
+		path: '/:user/sessions',
+		name: 'sessions',
+		component: () => import('@/views/both/SessionList.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/:user/session/:session_id',
+		name: 'session',
+		component: () => import('@/views/both/ViewSession.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/:user/session/:session_id/stage/1',
+		name: 'holding-deposit',
+		component: () => import('@/views/both/HoldingDeposit.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/:user/session/:session_id/stage/2',
+		name: 'background-screening',
+		component: () => import('@/views/both/BackgroundScreening.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/:user/session/:session_id/stage/3',
+		name: 'sales-contract',
+		component: () => import('@/views/both/SalesContract.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/:user/session/:session_id/stage/4',
+		name: 'escrow',
+		component: () => import('@/views/both/Escrow.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/:user/notifications',
+		name: 'notifications',
+		component: () => import('@/views/both/Notifications.vue'),
+		beforeEnter: logged_in
+	},
 
 	// Registration
 	{
@@ -58,7 +98,53 @@ export const routes = [
 	{
 		path: '/select-profile',
 		name: 'select-profile',
-		component: () => import('@/views/both/Profile_Selection.vue'),
+		component: () => import('@/views/both/ProfileSelection.vue'),
+		beforeEnter: logged_in
+	},
+
+	// Buyer Routes
+	{
+		path: '/buyer/search',
+		name: 'property-search',
+		component: () => import('@/views/buyer/PropertySearch.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/buyer/search/results',
+		name: 'search-results',
+		component: () => import('@/views/buyer/SearchResults.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/buyer/search/results/property/:property_id',
+		name: 'buyer-view-property',
+		component: () => import('@/views/both/ViewProperty.vue'),
+		beforeEnter: logged_in
+	},
+
+	// Seller Routes
+	{
+		path: '/seller/properties',
+		name: 'property-list',
+		component: () => import('@/views/seller/PropertyList.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/seller/properties/add',
+		name: 'add-property',
+		component: () => import('@/views/seller/AddProperty.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: '/seller/properties/:property_id',
+		name: 'seller-view-property',
+		component: () => import('@/views/both/ViewProperty.vue'),
+		beforeEnter: logged_in
+	},
+	{
+		path: 'seller/property/:property_id/verify',
+		name: 'verify-property',
+		component: () => import('@/views/seller/VerifyProperty.vue'),
 		beforeEnter: logged_in
 	}
 ]
