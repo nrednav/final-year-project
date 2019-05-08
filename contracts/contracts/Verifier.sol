6f9b5d28b08b0b0f881331f376b2212eb4bc3079
nrednav <nrednav@gmail.com>
2019-05-08 16:03:02 +0100

    Modified verifier contract    -> Owners name + title deed are hashed together to form a    property_uid    -> Smart contract "verify" function takes this property uid as input    instead of name + title deed hash for better security    -> Smart contract no longer has a respond/fill function to emit the    verification result because in my app the oracle will send a request    to the server to take care of this

--- contracts/contracts/Verifier.sol
+++ contracts/contracts/Verifier.sol
@@ -3,25 +3,12 @@ pragma solidity 0.5.7;
 contract Verifier {
 
 	// Events
-	event verification_requested(bytes32 title_deed_hash,
-										  string owner_forename,
-										  string owner_surname);
-
-	event verification_completed(bytes32 title_deed_hash,
-										  bool result);
+	event verification_requested(bytes32 property_uid);
 
 	// Functions
-	function verify(bytes32 _title_deed_hash,
-						 string memory _owner_forename,
-						 string memory _owner_surname)
+	function verify(bytes32 _property_uid)
 	public
 	{
-		emit verification_requested(_title_deed_hash, _owner_forename, _owner_surname);
-	}
-
-	function verified(bytes32 _title_deed_hash, bool _result)
-	public
-	{
-		emit verification_completed(_title_deed_hash, _result);
+		emit verification_requested(_property_uid);
 	}
 }
--- contracts/test/verifier_test.js
+++ contracts/test/verifier_test.js
@@ -9,23 +9,16 @@ contract("Verifier", accounts => {
 	});
 
 	it("should emit an event when verification is requested", async () => {
-		let title_deed_hash = "0xdc345837d24517858368a28c2022936404ae5a64e78dcac16331108d53eeca9c";
-		let owner_forename = "Bob";
-		let owner_surname = "Jenkins";
+		let property_uid = "0xdc345837d24517858368a28c2022936404ae5a64e78dcac16331108d53eeca9c";
 
-		let result = await verifier_instance.verify(title_deed_hash, owner_forename, owner_surname);
+		let result = await verifier_instance.verify(property_uid);
+
 		assert.equal(result.logs[0].event, "verification_requested");
-		assert.equal(result.logs[0].args.title_deed_hash, title_deed_hash, "Incorrect title deed hash emitted");
-		assert.equal(result.logs[0].args.owner_forename, owner_forename, "Incorrect owner forename emited");
-		assert.equal(result.logs[0].args.owner_surname, owner_surname, "Incorrect owner surname emitted");
 	});
 
-	it("should emit an event when verification is completed", async () => {
-		let title_deed_hash = "0xdc345837d24517858368a28c2022936404ae5a64e78dcac16331108d53eeca9c";
-
-		let result = await verifier_instance.verified("0xdc345837d24517858368a28c2022936404ae5a64e78dcac16331108d53eeca9c", true);
-		assert.equal(result.logs[0].event, "verification_completed");
-		assert.equal(result.logs[0].args.title_deed_hash, title_deed_hash, "Incorrect title deed emitted");
-		assert.equal(result.logs[0].args.result, true, "Incorrect verification result");
+	it("should emit the property_uid as part of the event", async () => {
+		let property_uid = "0xdc345837d24517858368a28c2022936404ae5a64e78dcac16331108d53eeca9c";
+		let result = await verifier_instance.verify(property_uid);
+		assert.equal(result.logs[0].args.property_uid, property_uid);
 	});
 });
