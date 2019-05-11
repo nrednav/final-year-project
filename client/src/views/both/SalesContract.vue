62c4eb25d40610faf4f43e9967efe2e3714ed35c
nrednav <nrednav@gmail.com>
2019-05-11 19:36:39 +0100

    Started sales contract interface Installed ipfs-http-client Modified hdOracle to only 'findOne' session instead of previously using 'find' which would return an array of sessions

---
+++ client/src/views/both/SalesContract.vue
@@ -0,0 +1,142 @@
+<template>
+	<v-layout column class="main-sc-container text-xs-center">
+
+		<h1 class="display-3 p_text--text font-weight-bold">
+			Stage 3: Sales Contract
+		</h1>
+
+		<div class="sc-card-container">
+			<v-card class="primary p_text--text pa-4 sc-card display-1">
+
+			</v-card>
+		</div>
+
+	</v-layout>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import axios from 'axios'
+
+export default {
+	data () {
+		return {
+		}
+	},
+
+	methods: {
+
+		updateSession (screeningUID, result) {
+			var requestUrl = `http://localhost:3000/api/sessions/${this.session._id}/update`
+			var config = { headers: { Authorization: 'a1b2c3d4e5f6g7' } }
+			var body = {
+				updateOptions: {
+					$set: {
+						'stages.2.buyer_screening_uid': screeningUID,
+						'stages.2.verification_status': result,
+						'stages.2.status': 'Completed'
+					}
+				}
+			}
+
+			axios.put(requestUrl, body, config).then((response) => {
+				console.log(response)
+				this.verificationRequested = true
+			}).catch((error) => {
+				console.log(error)
+			})
+		},
+
+		goBack () {
+			this.$router.go(-1)
+		}
+	},
+
+	computed: {
+		...mapGetters(['session', 'user_type', 'user_object'])
+	},
+
+	mounted () {
+	}
+}
+</script>
+
+<style>
+
+.main-sc-container {
+	display: grid;
+	grid-template-columns: repeat(1, 1fr);
+	justify-items: center;
+	align-items: center;
+	overflow-wrap: break-word;
+	word-wrap: break-word;
+	hyphens: auto;
+}
+
+.sc-card-container {
+	margin-top: 10vh;
+	width: 90%;
+}
+
+.sc-card {
+	border-radius: 25px;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-template-rows: repeat(4, minmax(10vh, auto));
+	column-gap: 10vmax;
+	row-gap: 2vmax;
+	align-items: center;
+	justify-items: start;
+}
+
+.bs-screening-uid {
+	grid-column: 1 / 3;
+	grid-row: 1;
+	width: 100%;
+	height: 100%;
+}
+
+.bs-verification-status {
+	grid-column: 1 / 3;
+	grid-row: 2;
+	width: 100%;
+	height: 100%;
+}
+
+.bs-btnBack {
+	grid-column: 1;
+	grid-row: 3;
+	height: 10vh;
+	width: 25vw;
+	border-radius: 10px;
+}
+
+.bs-btnPayDeposit {
+	grid-column: 2 / 3;
+	grid-row: 3;
+	height: 10vh;
+	width: 25vw;
+	border-radius: 10px;
+}
+
+.bss-button-container {
+	justify-items: center;
+	grid-column: 1 / 3;
+	width: 100%;
+}
+
+.bsb-button-container {
+	grid-column: 1 / 3;
+	width: 100%;
+}
+
+.disable-click {
+	pointer-events: none;
+	opacity: 0.5;
+}
+
+.bs-uid-value {
+	grid-column: 2;
+}
+
+</style>
