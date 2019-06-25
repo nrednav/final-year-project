5699f3695e4324e0a9f53bd8f9422b416434bbb5
nrednav <nrednav@gmail.com>
2019-06-25 02:56:31 +0100

    Fixed vulnerabilities with some dependencies -> Replaced Jade with Pug -> Fixed problem with line endings in network directory

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ var app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // Add Middleware
 app.use(logger);
